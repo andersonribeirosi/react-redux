95f6406af3f92156cefc55f17c0ab349ee2d391e
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-02-01 16:51:10 -0300

    Criação do projeto produtos + CRUD

--- react-redux-boot-mat/src/App.js
+++ react-redux-boot-mat/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome: ''
+  }
+
+  alterarNome = (event) => {
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+  criaComboBox = () => {
+    const opcoes = ['Anderson Ribeiro', 'Juliana Borges', 'Jaqueline Borges']
+    const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  componentDidMount(){
+    console.log("Executou o DidMount");
+    
+  }
+
+  render() {
+    console.log("Executou o render");
+    
+
+    //Outra forma de chamar uma função (Criando um componente e chamando em uma tag)
+    const MeuComboBox = () => this.criaComboBox()
+
+    return (
+      // (React.Fragment) Permitir inserir varias linhas de HTML ou apenas <> </>
+      <React.Fragment>
+        <input className="text-centralizado" type="text" value={this.state.nome} onChange={this.alterarNome}></input>
+        <h1>Nome: {this.state.nome}</h1>
+        {this.criaComboBox()}
+        <MeuComboBox></MeuComboBox>
+      </React.Fragment>
+    )
+  }
+
 }
 
 export default App;
