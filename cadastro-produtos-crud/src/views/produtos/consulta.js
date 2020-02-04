40ed4134d8f525afd3732dd4c726c6a55461c0e7
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-02-04 18:15:00 -0300

    Componetizando a lista de produtos - ProdutosTable

--- cadastro-produtos-crud/src/views/produtos/consulta.js
+++ cadastro-produtos-crud/src/views/produtos/consulta.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ProdutoService from '../../app/produtoService'
+import Card from '../../components/card'
+import ProdutosTable from '../produtos/produtosTable'
 
 import { withRouter } from 'react-router-dom'
 
@@ -9,66 +11,34 @@ class ConsultaProdutos extends React.Component {
         produtos: []
     }
 
-    constructor(){
+    constructor() {
         super()
         this.service = new ProdutoService();
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const produtos = this.service.obterProdutos()
-        this.setState({produtos})
+        this.setState({ produtos })
     }
 
     editar = (sku) => {
         console.log('Editando o sku: ', sku);
-        
+
         this.props.history.push(`/cadastro-produtos/${sku}`)
     }
 
     deletar = (sku) => {
-       const produtos = this.service.deletar(sku)
-       this.setState({produtos})
+        const produtos = this.service.deletar(sku)
+        this.setState({ produtos })
     }
 
     render() {
         return (
-
-            <div className="card">
-            <div className="card-header">
-                Consulta de Produtos
-            </div>
-            <div className="card-body">
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>SKU</th>
-                        <th>Preço</th>
-                        <th>Fornecedor</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.produtos.map((produto, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th>{produto.nome}</th>
-                                    <th>{produto.sku}</th>
-                                    <th>{produto.preco}</th>
-                                    <th>{produto.fornecedor}</th>
-                                    <th>
-                                        <button onClick={() => this.editar(produto.sku)} className="btn btn-primary mr-3">Editar</button>
-                                        <button onClick={() => this.deletar(produto.sku)} className="btn btn-danger">Excluir</button>
-                                    </th>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-            </div>
-            </div>
+            <Card header="Consulta Produtos">
+                <ProdutosTable produtos={this.state.produtos}
+                    editarAction={this.editar}
+                    deletarAction={this.deletar} />
+            </Card>
         )
     }
 }
